a036f3db085461bd2d07e58e052367610e327090
Santiago Denis <santidenis.sd@gmail.com>
2021-09-11 22:18:47 -0300

    [ADD] Class 4 - functions

--- js/main.js
+++ js/main.js
@@ -1,79 +1,144 @@
+// Algoritmo: ¿Cuál es tu edad o peso en otros planetas?
 
-// Algoritmo: ¿Cuantos años humanos tiene tu mascota en comparación tuya?
-//Al algoritmo de la clase anterior, se le han agregado condicionales para que se pueda calcular no solo la edad de perros, sino tambien de otras 6 mascotas.
+//Const para calcular peso
+const mercuryGravity = 3.7;
+const venusGravity = 8.8;
+const earthGravity = 9.8;
+const marsGravity = 3.7;
+const jupiterGravity = 24.8;
+const saturnGravity = 10.4;
+const uranusGravity = 8.8;
+const neptuneGravity = 11.1;
 
-let dog = 1 //la esperanza de vida promedio de un perro es de 11 años y la esperanza de vida promedio humana es de 77. La edad del perro se multiplica *7 para ser un año humano.
-let cat = 2 // el cálculo se torna más complejo: el primer año de vida del gato equivale a 15 años de edad en humanos, mientras que el segundo año solo equivale a 10 años humanos. Cuando el gato es completamente adulto, a partir aproximadamente del 3er año, cada año gatuno equivale a 4 años humanos.
-let turtle = 3 //una tortuga doméstica vive 20 años apróx (esto sin incluir tortugas "libres" marinas o terrestres que tienen una esperanza de vida de 100 años). Por lo que un año de tortuga son 4 humanos.
-let hamster = 4 //la esperanza de vida de un hámster no pasa de 4 años; por lo que un año de hámster equivale a 19 años humanos.
-let bird = 5 //considerando las aves domésticas solamente, como un canario, tienen una esperanza de vida de 10 a 11 años en condiciones optimas. Por lo que reutilizaremos el código para calcular perros.
-let fish = 6 //existe una variada cantidad de peces que pueden ser adoptados como mascota. No obstante, considerando los más populares (goldfish, guppy, pez payaso, etc), tienen una esperanza de vida de 2 años. (38 años humanos = 1 año de pez).
-let rabbit = 7//un conejo domestico tiene 9 años de esperanza de vida promedio.  Un año humano = 9 años de conejo.
+//const para calcular edad
+const mercuryAge = .24;
+const venusAge = .61;
+const earthAge = 1;
+const marsAge = 1.88;
+const jupiterAge = 11.86;
+const saturnAge = 29.47;
+const uranusAge = 84.07;
+const neptuneAge = 164.88;
 
-let userName = prompt("Ingrese su nombre: "); //Nombre del usuario
+var userName = prompt("Ingrese su nombre: "); //Nombre del usuario
 
-let welcome = parseInt(prompt("Hola " + userName + "! Si quieres continuar el quiz presiona el botón aceptar, de lo contrario, presiona 9")); //let que utilizaremos al finar del while para definir un camino
+let welcome = numberConvertor( prompt("Hola " + userName + "! Si quieres saber tu peso en otros planetas pulsa 1, si quieres saber tu edad en otros planetas pulsa 2, para salir, presiona el boton aceptar")); //let que utilizaremos al finar del while para definir un camino
 
-let equivalent = "";
+var result = "";
 
-let result = "";
+//while de peso. Aquí uso una regla de tres simple llamando la función weightOperation que calcula el peso terrestre de la persona, lo procesa y devuelve un valor segun lo introducido de la misma en base al planeta que haya escogido.
+while (welcome == 1) {
 
-while(welcome != 9, welcome != 1) { //comienzo del while que excluye dos numeros para que el usuario pueda elegir un camino (los detalles se encuentran debajo, junto al let welcome)
+var weight = numberConvertor( prompt("Ingrese su peso en kg aquí: ")); //etapa 1
 
-    let petName = prompt(userName + ", " + "ingresa el nombre de tu mascota: "); //nombre de la mascota
+var planet = numberConvertor( prompt(userName + " por favor, introducí el planeta en el que quieres saber tu peso, siendo 1 para mercurio, 2 para venus, 3 para marte, 4 para jupiter, 5 para saturno, 6 para urano y 7 para neptuno ")) //etapa 2
 
-    let petKind = parseInt(prompt(userName + " por favor, introducí el tipo de mascota que es " + petName + ", siendo 1 para perro, 2 para gato, 3 para tortuga, 4 para hámster, 5 para pájaro, 6 para pez y 7 para conejo "));
-
-    let petAge = parseInt(prompt("Ahora, ingresa la edad de " + petName)); //edad de la mascota
-
-    switch (petKind) { //switch que cumple un rol similar al if que se encontraba previamente, solo que esta un un bucle
+    switch (planet) { //etapa 3
         case 1:
-            result = petAge * 7;
-            alert (petName + " tiene " + result + " años humanos ;)");
+            result = mercuryGravity;
+            weightOperation()
+            alert (userName + ", tienes " + weightOperation() + " kg en Mercurio! ;)");
             break;
-        case 2:
-            if (petAge == 1) {
-                result = petAge * 15;
-                alert (petName + " tiene " + result + " años humanos ;)");
-                break;
-            }else if (petAge == 2) {
-                result = ((petAge - 1) * 10) + 15;
-                alert (petName + " tiene " + result + " años humanos ;)");
-                break;
-            }else {
-                result = ((petAge - 2) * 4) + 25;
-                alert (petName + " tiene " + result + " años humanos ;)");
-                break;
-            }
-        case 3:
-            result = petAge * 4;
-            alert (petName + " tiene " + result + " años humanos ;)");
+            case 2:
+            result = venusGravity;
+            weightOperation()
+            alert (userName + ", tienes " + weightOperation() + " kg en Venus! ;)");
+            case 3:
+            result = marsGravity;
+            weightOperation()
+            alert (userName + ", tienes " + weightOperation() + " kg en Marte! ;)");
             break;
-        case 4:
-            result = petAge * 19;
-            alert (petName + " tiene " + result + " años humanos ;)");
+            case 4:
+            result = jupiterGravity;
+            weightOperation()
+            alert (userName + ", tienes " + weightOperation() + " kg en Júpiter! ;)");
             break;
-        case 5:
-            result = petAge * 7;
-            alert (petName + " tiene " + result + " años humanos ;)");
+            case 5:
+            result = saturnGravity;
+            weightOperation()
+            alert (userName + ", tienes " + weightOperation() + " kg en Saturno! ;)");
             break;
-        case 6:
-            result = petAge * 38;
-            alert (petName + " tiene " + result + " años humanos ;)");
+            case 6:
+            result = uranusGravity;
+            weightOperation()
+            alert (userName + ", tienes " + weightOperation() + " kg en Urano! ;)");
             break;
-        case 7:
-            result = petAge * 9;
-            alert (petName + " tiene " + result + " años humanos ;)");
+            case 7:
+            result = neptuneGravity;
+            weightOperation()
+            alert (userName + ", tienes " + weightOperation() + " kg en Neptuno! ;)");
             break;
-        default:
+            default:
             alert ("No has ingresado el dato correcto :(");
             break;
-    }
-    welcome = parseInt(prompt("¡Bien hecho " + userName + "! " + "Ahora, ingresa 9 para salir, 1 si quieres ver la edad que tendrá tu mascota por diez años en el futuro, o toca el botón aceptar para seguir viendo la edad de más animales!"));//aquí esta el uso del let welcome para que elija un camino el usuario. En caso de que elija 9 saldrá, si elije 1 irá al for de abajo o sino continua con el bucle para poner otra mascota.
-    if (welcome == 1) { //se encuentra el if que permite entrar al for
-        for (let i = 1; i <= 10; i++) { //el for que hace un calculo para encontrar la edad de la mascota previamente sellecionada en un futuro
-            alert ("Dentro de " + i + " años, tu mascota tendrá " + ((result / petAge) * i + result) + " años de edad");
         }
+        welcome = numberConvertor(prompt("¡Bien hecho " + userName + "! " + "Ahora, ingresa el boton aceptar para salir,pulsa 2 para ver tu edad en otros planetas o pulsa 1 para seguir viendo tu peso en otros planetas!"));
     }
-}
 
+//while de edad. Utilizo una regla de tres simple de la funcion ageOperation para asi calcular la edad del usuario equivalente al planeta que elijió
+while (welcome == 2) {
+
+    var age = numberConvertor( prompt("Ingrese sus años de edad aquí: ")); //etapa 1
+
+    var planet = numberConvertor( prompt(userName + " por favor, introducí el planeta en el que quieres saber tu edad, siendo 1 para mercurio, 2 para venus, 3 para marte, 4 para jupiter, 5 para saturno, 6 para urano y 7 para neptuno ")) //etapa 2
+
+        switch (planet) { //etapa 3
+            case 1:
+                result = mercuryAge;
+                ageOperation()
+                alert (userName + ", tienes " + ageOperation() + " años de edad en Mercurio! ;)");
+                break;
+                case 2:
+                result = venusAge;
+                ageOperation()
+                alert (userName + ", tienes " + ageOperation() + " años de edad en Venus! ;)");
+                case 3:
+                result = marsAge;
+                ageOperation()
+                alert (userName + ", tienes " + ageOperation() + " años de edad en Marte! ;)");
+                break;
+                case 4:
+                result = jupiterAge;
+                ageOperation()
+                alert (userName + ", tienes " + ageOperation() + " años de edad en Júpiter! ;)");
+                break;
+                case 5:
+                result = saturnAge;
+                ageOperation()
+                alert (userName + ", tienes " + ageOperation() + " años de edad en Saturno! ;)");
+                break;
+                case 6:
+                result = uranusAge;
+                ageOperation()
+                alert (userName + ", tienes " + ageOperation() + " años de edad en Urano! ;)");
+                break;
+                case 7:
+                result = neptuneAge;
+                ageOperation()
+                alert (userName + ", tienes " + ageOperation() + " años de edad en Neptuno! ;)");
+                break;
+                default:
+                alert ("No has ingresado el dato correcto :(");
+                break;
+            }
+            welcome = numberConvertor(prompt("¡Bien hecho " + userName + "! " + "Ahora, ingresa aceptar para salir, pulsa 1 para ver tu peso en otros planetas o pulsa 2 para seguir viendo tu edad en otros planetas!"));
+        }
+
+//las tres funciones que solicitan el desafío complementario
+   function weightOperation() {
+        const planetWeight = weight * result / earthGravity;
+        return planetWeight;
+   }
+
+   function ageOperation() {
+       const planetAge = earthAge * age / result;
+        return planetAge;
+   }
+
+    function numberConvertor(pNumber) {
+        const isANumber = !isNaN(pNumber);
+        if (isANumber) {
+            return parseInt(pNumber);
+        }
+        return 0;
+    }
